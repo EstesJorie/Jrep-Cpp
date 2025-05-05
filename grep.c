7a0bc47ca153504627812fb3c4ce32f0856cc94c
EstesJorie <joe.tresise@aol.com>
2025-05-05 12:53:58 +0100

    addition of regex matching in Jrep

--- grep.c
+++ grep.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <regex.h>
+#include <stdlib.h>
 
 #define MAX_LINE 1024
+
+void printHighlighted(const char *line, regmatch_t match)
+{
+    fwrite(line, 1, match.rm_so, stdout); //before match
+
+    printf("\033[1;31m"); //red color
+    fwrite(line + match.rm_so, 1, match.rm_eo - match.rm_so, stdout);
+    printf("\033[0m"); //reset color
+
+    fputs(line + match.rm_eo, stdout); //rest of line printed
+}
 
 int main(int argc, char *argv[]) //argc = typed words, *argv = place of words
 {
@@ -20,14 +34,29 @@ int main(int argc, char *argv[]) //argc = typed words, *argv = place of words
         return 1; //error opening
     }
 
+    regex_t regex;
+    int reti = regcomp(&regex, pattern, REG_ICASE | REG_EXTENDED); //compile pattern
+    if (reti)
+    {
+        fprintf(stderr, "Could not compile regex\n");
+        return 1;
+    }
+    
     char line[MAX_LINE];
+    int lineNum = 0;
     while (fgets(line, sizeof(line), fp))
     {
-        if(strstr(line, pattern))
+        lineNum++;
+
+        regmatch_t match;
+        if (regexec(&regex, line, 1, &match, 0) == 0)
         {
-            fputs(line, stdout); //print line where word is contained
+            printf("%4d: ", lineNum); //show line num
+            printHighlighted(line, match); //print match
         }
     }
+
+    regfree(&regex); //frees memory from regex
     if(fp != stdin) fclose(fp);
     return 0;
 }
